38a0083842462c37b3f40436671728241efa79fb
Nyolczas <nyolczas@gmail.com>
2018-12-18 23:09:07 +0100

    pig game

--- pigGame/app.js
+++ pigGame/app.js
@@ -8,3 +8,62 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+// pontszám lekérdezése
+var x = document.querySelector('#score-0').textContent; 
+console.log(x);
+
+// eltünteti a kezdőképről a dobókockát
+document.querySelector('.dice').style.display = 'none';
+
+// lenullázza az értékeket
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+// dobókocka dobás funkció
+document.querySelector('.btn-roll').addEventListener('click', function() {
+   
+    // 1. random number
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        // add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    } else {
+        // next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+
+        diceDOM.style.display = 'none';
+    }
+
+});
+
+
+
