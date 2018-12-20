64ac536a285dd04a495eef16374ef3eede8e12bf
Nyolczas <nyolczas@gmail.com>
2018-12-20 18:27:50 +0100

    hamburger menu

---
+++ animationYoutube/trigger01.js
@@ -0,0 +1,7 @@
+function change(x) {
+    var element = document.getElementById('box1');
+
+    x.classList.toggle("change");
+    document.querySelector('#box1').classList.toggle('anima');
+    document.querySelector('#box1').classList.toggle('nemani');
+}
