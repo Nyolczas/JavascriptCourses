4c362ab007b91ce4f3ee8b865758df67c112063e
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2019-01-09 16:50:47 +0100

    Update app.js

--- myBudgety/app.js
+++ myBudgety/app.js
@@ -63,7 +63,9 @@ var UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expenseContainer: '.expenses__list'
     };
 
     return { // publikussá teszi a változókat
@@ -76,39 +78,25 @@ var UIController = (function () {
         },
 
         addListItem: function (obj, type) {
-            var html, newHtml;
+            var html, newHtml, element;
 
             // HTML placeholder stringek készítése
             if (type === 'inc') {
-                html = '<div class="item clearfix" id="income-%id%">\
-                            <div class="item__description">%description%</div>\
-                                <div class="right clearfix">\
-                                    <div class="item__value">%value%</div>\
-                                    <div class="item__delete">\
-                                        <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>\
-                                    </div>\
-                                </div>\
-                            </div>';
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
-                html = '<div class="item clearfix" id="expense-%id%"">\
-                            <div class="item__description">%description%</div>\
-                            <div class="right clearfix">\
-                                <div class="item__value">%value%</div>\
-                                <div class="item__percentage">21%</div>\
-                                <div class="item__delete">\
-                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>\
-                                </div>\
-                            </div>\
-                        </div>';
+                element = DOMstrings.expenseContainer;
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
 
             // A placeholderek lecserélése az aktuális adatokra
-            newHtml = html.replace('%id%',obj.id);
+            newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%',obj.description);
             newHtml = newHtml.replace('%value%',obj.value);
 
             // A HTML beszúrása a DOM-ba
-            
+            // help --- https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
         getDomstrings: function () { // publikussá teszi a DOMstrings változókat
@@ -149,6 +137,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         //- 3. Elemek hozzáadása az UI-hoz.
+        UICtrl.addListItem(newItem, input.type);
 
         //- 4. Budget kiszámolása
 
