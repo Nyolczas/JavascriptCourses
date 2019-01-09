31d56111c0d7e285f8a593579ffffcaa43a93ce0
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2019-01-09 11:24:16 +0100

    Update app.js

--- myBudgety/app.js
+++ myBudgety/app.js
@@ -1,11 +1,10 @@
-
 //--- BUDGET CONTROLLER
-var budgetController = (function() {
+var budgetController = (function () {
 
 })();
 
 //--- UI CONTROLLER
-var UIController = (function(){
+var UIController = (function () {
 
     var DOMstrings = { // ez a lista gyűjti a bemeneti sztingeket
         inputType: '.add__type',
@@ -15,53 +14,63 @@ var UIController = (function(){
     };
 
     return { // publikussá teszi a változókat
-        getInput: function(){ // begyűjti a beadott adatokat.
+        getInput: function () { // begyűjti a beadott adatokat.
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // inc vagy exp lehet
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         },
-        
-        getDomstrings: function() { // publikussá teszi a DOMstrings változókat
+
+        getDomstrings: function () { // publikussá teszi a DOMstrings változókat
             return DOMstrings;
         }
     };
 })();
 
 //--- GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDomstrings();
+    // eseménykezelők gyűjteménye
+    var setupEventListeners = function () { 
+        var DOM = UICtrl.getDomstrings(); // behívja a DOM elemeket
 
-    var ctrlAddItem = function() {
+        // a gomb megnyomása meghívja a ctrlAddItem függvényt.
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        // az enter leütése meghívja a ctrlAddItem függvényt.
+        document.addEventListener('keypress', function (event) {
+            // console.log(event);
+            if (event.keyCode === 13 || event.witch === 13) {
+                // console.log('Entert nyomtál!');
+                ctrlAddItem();
+            }
+        });
+    };
+
+    var ctrlAddItem = function () {
         //console.log('Működik!');
 
-         //- 1. Beadott adatok felvétele
-         var input = UICtrl.getInput();
-         console.log(input);
-    
+        //- 1. Beadott adatok felvétele
+        var input = UICtrl.getInput();
+        console.log(input);
+
         //- 2. Elemek átadása a Budget controllernek
-    
+
         //- 3. Elemek hozzáadása az UI-hoz.
-    
+
         //- 4. Budget kiszámolása
-    
+
         //- 5. Budget megjelenítése
+    };
 
-    }
-    // a gomb megnyomása meghívja a ctrlAddItem függvényt.
-    document.querySelector(inputBtn).addEventListener('click', ctrlAddItem); 
-
-    // az enter leütése meghívja a ctrlAddItem függvényt.
-    document.addEventListener('keypress', function(event){  
-        // console.log(event);
-        if (event.keyCode === 13 || event.witch === 13) {
-            // console.log('Entert nyomtál!');
-            ctrlAddItem();
+    return { // a setupEventListeners függvény pulikussá tétele.
+        init: function() { 
+            console.log('Az alkalmazás elindult.');
+            setupEventListeners();
         }
-
-    });
+    };
 
 })(budgetController, UIController);
 
+controller.init();
