2ca88410d4a86dd438bf6ec79cbb9674685396ac
Nyolczas <nyolczas@gmail.com>
2019-01-12 20:25:22 +0100

    ES6 string

--- objects.js
+++ objects.js
@@ -9,10 +9,14 @@
      family: ['Jane', 'Mark', 'Bob', 'Emily'],
      job: 'teacher',
      isMarried: false,
+     
      calcAge: function() {
-         this.age = 2018 - this.birthYear;
+         var now = new Date();
+         var thisYear = now.getFullYear();
+         this.age = thisYear - this.birthYear;
+         return this.age;
      }
  };
 
- console.log(john.calcAge(1990));
+ console.log(`John born in ${john.birthYear} , and he is ${john.calcAge(john.birthYear)} years old.`);
  
